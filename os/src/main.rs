75ee6b254ed0ebdc1915811efa1015137a23f2a8
night <1554116967@qq.com>
2024-01-25 13:43:33 +0800

    ch2 init

---
+++ os/src/main.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+#![feature(panic_info_message)]
+#[macro_use]
+mod console;
+mod batch;
+mod lang_item;
+mod sbi;
+mod sync;
+mod syscall;
+mod trap;
+use core::arch::global_asm;
+
+global_asm!(include_str!("./entry.asm"));
+global_asm!(include_str!("./link_app.S"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    trap::init();
+    batch::init();
+    batch::run_next_app();
+    // test_code();
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe {
+        (a as *mut u8).write_volatile(0);
+    });
+}
---
+++ user/src/lib.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![feature(linkage)]
+#![feature(panic_info_message)]
+
+#[macro_use]
+pub mod console;
+mod lang_item;
+mod syscall;
+
+#[no_mangle]
+#[link_section = ".text.entry"]
+pub extern "C" fn _start() -> ! {
+    clear_bss();
+    exit(main());
+    panic!("Unreachable in _start!");
+}
+
+#[linkage = "weak"]
+#[no_mangle]
+fn main() -> i32 {
+    panic!("Cannot find main!");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn start_bss();
+        fn end_bss();
+    }
+    (start_bss as usize..end_bss as usize).for_each(|a| unsafe {
+        (a as *mut u8).write_volatile(0);
+    });
+}
+
+pub fn write(fd: usize, buffer: &[u8]) -> isize {
+    syscall::sys_write(fd, buffer)
+}
+
+pub fn exit(code: i32) -> isize {
+    syscall::sys_exit(code)
+}
