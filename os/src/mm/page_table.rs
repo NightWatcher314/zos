09319d97a85f9098aa87294560a4c3047384d6e9
night <1554116967@qq.com>
2024-01-29 20:56:11 +0800

    page_table

--- os/src/mm/page_table.rs
+++ os/src/mm/page_table.rs
@@ -1,6 +1,8 @@
+use crate::mm::address::*;
 use bitflags::*;
 
 bitflags! {
+    #[derive(Default, PartialEq, Eq)]
     pub struct PTEFlags: u8 {
         const V = 1 << 0;// valid
         const R = 1 << 1;// readable
@@ -17,4 +19,38 @@ bitflags! {
 #[repr(C)]
 pub struct PageTableEntry {
     pub bits: usize,
+}
+
+impl PageTableEntry {
+    pub fn new(ppn: PhysPageNum, flags: PTEFlags) -> Self {
+        Self {
+            bits: (ppn.0 << 10) | flags.bits() as usize,
+        }
+    }
+
+    pub fn empty() -> Self {
+        Self { bits: 0 }
+    }
+
+    pub fn ppn(&self) -> PhysPageNum {
+        (self.bits >> 10 & (1 << 44 - 1)).into()
+    }
+
+    pub fn flags(&self) -> PTEFlags {
+        PTEFlags::from_bits_truncate(self.bits as u8)
+    }
+
+    pub fn valid(&self) -> bool {
+        (self.flags() & PTEFlags::V) != PTEFlags::empty()
+    }
+
+    pub fn readable(&self) -> bool {
+        (self.flags() & PTEFlags::R) != PTEFlags::empty()
+    }
+    pub fn writable(&self) -> bool {
+        (self.flags() & PTEFlags::W) != PTEFlags::empty()
+    }
+    pub fn executable(&self) -> bool {
+        (self.flags() & PTEFlags::X) != PTEFlags::empty()
+    }
 }
